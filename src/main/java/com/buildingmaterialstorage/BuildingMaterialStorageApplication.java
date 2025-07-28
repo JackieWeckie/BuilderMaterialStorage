7d0afa4f4555637bc7620b839d8307741d50320d
JackieWeckie <nekit4568i@gmail.com>
2025-07-28 14:20:44 +0300

    initial commit

---
+++ src/main/java/com/buildingmaterialstorage/BuildingMaterialStorageApplication.java
@@ -0,0 +1,13 @@
+package com.buildingmaterialstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingMaterialStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingMaterialStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buildingmaterialstorage/BuildingMaterialStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildingmaterialstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingMaterialStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
